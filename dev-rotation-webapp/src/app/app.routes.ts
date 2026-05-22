8803398b034259f5e1d51ee838ae575137771d2c
mgo738 <mikelmylgo@icloud.com>
2026-05-22 10:42:28 +0100

    Add skills ranking page

--- dev-rotation-webapp/src/app/app.routes.ts
+++ dev-rotation-webapp/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import {DetailsPageComponent} from './details-page/details-page.component';
 import {HomePageComponent} from './home-page/home-page.component';
 import {AboutMeComponent} from './about-me/about-me.component';
 import {UniversityComponent} from './university/university.component';
+import {SkillsRatingComponent} from './skills-rating/skills-rating.component';
 
 export const routes: Routes = [
   { path: 'details', component: DetailsPageComponent },
   { path: 'home', component: HomePageComponent },
   { path: 'about-me', component: AboutMeComponent },
   { path: 'university', component: UniversityComponent },
+  { path: 'skills-rating', component: SkillsRatingComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
---
+++ dev-rotation-webapp/src/app/skills-rating/skills-rating.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills-rating',
+  imports: [],
+  templateUrl: './skills-rating.component.html',
+  styleUrl: './skills-rating.component.scss'
+})
+export class SkillsRatingComponent {
+
+}
