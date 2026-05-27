a690397bf1203ec84fd3c6078c8f6a812b062abf
mgo738 <mikelmylgo@icloud.com>
2026-05-27 13:50:07 +0100

    Populate pages with more information

--- dev-rotation-webapp/src/app/about-me/about-me.component.ts
+++ dev-rotation-webapp/src/app/about-me/about-me.component.ts
@@ -8,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AboutMeComponent {
   age = 19;
+  today = new Date();
+  startDate = new Date("2025-09-15T00:00:00.000Z")
+
+  atResonate = new Date(this.today.getTime() - this.startDate.getTime())
+
+  totalDaysAtResonate = Math.floor((this.today.getTime() - this.startDate.getTime()) / (8.64*10**7))
+  totalHoursAtResonate = Math.floor((this.today.getTime() - this.startDate.getTime()) / (3.6*10**6))
+  totalMinutesAtResonate = Math.floor((this.today.getTime() - this.startDate.getTime()) / (60000))
+
+  daysAtResonate = this.atResonate.getDate() - 1
+  monthsAtResonate = this.atResonate.getMonth()
+  yearsAtResonate = this.atResonate.getFullYear() - 1970;
 }
